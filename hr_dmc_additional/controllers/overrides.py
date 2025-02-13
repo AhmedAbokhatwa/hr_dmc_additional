9b1db2b0540771d46439ad69e053814fde12b43a
AhmedAbokhatwa <ahmedreda199785@gmail.com>
2025-02-13 13:25:25 +0200

    create GL Entry check in component it triggers to make gl for salary slip component

--- hr_dmc_additional/controllers/overrides.py
+++ hr_dmc_additional/controllers/overrides.py
@@ -24,11 +24,13 @@ class CustomPayroll(PayrollEntry):
     # Overriding the method from the base class
     def should_add_component_to_accrual_jv(self, component_type: str, item: dict) -> bool:
         add_component_to_accrual_jv = True
-        do_not_include_in_total, is_flexible_benefit, only_tax_impact = frappe.get_cached_value(
-            "Salary Component", item["salary_component"], ["do_not_include_in_total", "is_flexible_benefit", "only_tax_impact"]
+        custom_create_gl_entry, is_flexible_benefit, only_tax_impact = frappe.get_cached_value(
+            "Salary Component", item["salary_component"], ["custom_create_gl_entry", "is_flexible_benefit", "only_tax_impact"]
         )
-        if cint(do_not_include_in_total):
-            add_component_to_accrual_jv = False   
+        if cint(custom_create_gl_entry):
+            add_component_to_accrual_jv = True
+        if not cint(custom_create_gl_entry):
+            add_component_to_accrual_jv = False       
         if component_type == "earnings":
             # Check if it's a flexible benefit and only has a tax impact
             if cint(is_flexible_benefit) and cint(only_tax_impact):
